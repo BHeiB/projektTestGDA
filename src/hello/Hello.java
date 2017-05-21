8163b654ac048dab9354446f19387660eb543f5d
Radoslaw Kordys <radekor1@wp.pl>
2017-05-21 12:54:29 +0200

    poprawka wyswietlanie daty

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,10 +1,14 @@
 package hello;
 
+import java.time.LocalDate;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Witaj œwiecie");
 		System.out.println("aha! Cos siê smieni³o");
+		System.out.println("dzis jest" + LocalDate.now());
 	}
+	
 
 }
