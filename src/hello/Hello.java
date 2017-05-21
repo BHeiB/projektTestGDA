961da6a5e821fff8dda5da4b1e4fc75f993574b2
Radoslaw Kordys <radekor1@wp.pl>
2017-05-21 12:55:53 +0200

    ostatnia wersja!

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Witaj œwiecie");
 		System.out.println("aha! Cos siê smieni³o");
+		System.out.println("koncze nowa funcjonalnosc");
 		
 	}
 
