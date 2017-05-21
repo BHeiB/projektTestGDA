137171b638150aa526a900fa6d9c8474585884cd
Radoslaw Kordys <radekor1@wp.pl>
2017-05-21 12:50:55 +0200

    nowa funkcjonlnosci wersja 22

--- src/hello/NowaFuckjonalnosc.java
+++ src/hello/NowaFuckjonalnosc.java
@@ -4,6 +4,7 @@ public class NowaFuckjonalnosc {
 
 	public static void main(String[] args) {
 		System.out.println("Super extra ale jeszcze trzeba pacy");
+		System.out.println("i cos dopisalem nawet! c'nie/1");
 
 	}
 
